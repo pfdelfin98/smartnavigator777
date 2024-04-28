c095d0bff78768e001388548ffc50c5e3c47f427
pfdelfin98 <pfdelfin98@gmail.com>
2024-04-28 18:41:22 +0800

    update get coordinates

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+# type: ignore
 from flask import Flask, jsonify, request
 from marshmallow import Schema, fields, ValidationError
 from datetime import datetime
@@ -5,19 +6,31 @@ from dotenv import load_dotenv
 import os
 import pytz
 
-
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
 manila_timezone = pytz.timezone('Asia/Manila')
 
-# Rest of your code remains the same
+class CoordinatesSchema(Schema):
+    latitude = fields.Float(required=True)
+    longitude = fields.Float(required=True)
+
+
+coordinates = {
+    'latitude': 14.0833428,
+    'longitude': 121.1440427,
+    'updated_at': datetime.now().isoformat(),
+}
+
+
+coordinates_schema = CoordinatesSchema()
+
 
 @app.route('/')
 def home():
     return "Hello World!"
+
 
 @app.route('/api/coordinates', methods=['GET', 'POST'])
 def handle_coordinates():
@@ -28,8 +41,8 @@ def handle_coordinates():
 
     elif request.method == 'POST':
         try:
-            # Uncomment the following block for authentication
             # secret_key = os.getenv('SECRET_KEY')
+
             # if request.headers.get('Secret-Key') != secret_key:
             #     return 'Unauthorized', 401
 
@@ -37,9 +50,11 @@ def handle_coordinates():
             coordinates.update(data)
             manila_time = datetime.now(manila_timezone).isoformat()
             coordinates['updated_at'] = manila_time
-            return 'Coordinates updated successfully', 200
+            print(coordinates)
+            return (coordinates, 200)
         except ValidationError as err:
             return str(err), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
